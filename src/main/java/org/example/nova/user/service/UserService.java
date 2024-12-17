be3b7c4e3e57f461bb894e92cd657278736d13dc
김한주 <s24043@gsm.hs.kr>
2024-12-17 22:42:40 +0900

    modify :: UserService.java

--- src/main/java/org/example/nova/user/service/UserService.java
+++ src/main/java/org/example/nova/user/service/UserService.java
@@ -1,12 +1,12 @@
-package org.example.nova.auth.service;
+package org.example.nova.user.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.example.nova.auth.entity.Member;
-import org.example.nova.auth.entity.MemberRole;
+import org.example.nova.user.entity.User;
+import org.example.nova.user.entity.UserRole;
 import org.example.nova.auth.presentation.dto.JoinRequest;
 import org.example.nova.auth.presentation.dto.LoginRequest;
-import org.example.nova.auth.repository.MemberRepository;
+import org.example.nova.user.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,36 +15,36 @@ import java.util.Optional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
-    private final MemberRepository memberRepository;
+public class UserService {
+    private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public boolean checkLoginIdDuplicate(String loginId) {
-        return memberRepository.existsByLoginId(loginId);
+        return userRepository.existsByLoginId(loginId);
     }
 
     public void join(JoinRequest joinRequest) {
-        memberRepository.save(joinRequest.toEntity());
+        userRepository.save(joinRequest.toEntity());
     }
 
-    public Member login(LoginRequest loginRequest) {
-        Member findMember = memberRepository.findByLoginId(loginRequest.getLoginId());
+    public User login(LoginRequest loginRequest) {
+        User findUser = userRepository.findByLoginId(loginRequest.getLoginId());
 
-        if(findMember == null){
+        if(findUser == null){
             return null;
         }
 
-        if (!findMember.getPassword().equals(loginRequest.getPassword())) {
+        if (!findUser.getPassword().equals(loginRequest.getPassword())) {
             return null;
         }
 
-        return findMember;
+        return findUser;
     }
 
-    public Member getLoginMemberById(Long memberId){
+    public User getLoginMemberById(Long memberId){
         if(memberId == null) return null;
 
-        Optional<Member> findMember = memberRepository.findById(memberId);
+        Optional<User> findMember = userRepository.findById(memberId);
         return findMember.orElse(null);
 
     }
@@ -54,32 +54,32 @@ public class MemberService {
         String password = joinRequest.getPassword();
         String name = joinRequest.getName();
 
-        if (memberRepository.existsByLoginId(loginId)) {
+        if (userRepository.existsByLoginId(loginId)) {
             throw new IllegalArgumentException("이미 존재하는 ID입니다.");
         }
 
         String encodedPassword = bCryptPasswordEncoder.encode(password);
 
-        Member newMember = Member.builder()
+        User newUser = User.builder()
                 .loginId(loginId)
                 .password(encodedPassword)
                 .name(name)
-                .role(MemberRole.USER)
+                .role(UserRole.USER)
                 .build();
 
-        memberRepository.save(new Member());
+        userRepository.save(new User());
     }
 
-    public Member authenticate(LoginRequest loginRequest) {
+    public User authenticate(LoginRequest loginRequest) {
         String loginId = loginRequest.getLoginId();
         String password = loginRequest.getPassword();
 
-        Member member = memberRepository.findByLoginId(loginId);
+        User user = userRepository.findByLoginId(loginId);
 
-        if (member == null || !bCryptPasswordEncoder.matches(password, member.getPassword())) {
+        if (user == null || !bCryptPasswordEncoder.matches(password, user.getPassword())) {
             return null;
         }
 
-        return member;
+        return user;
     }
 }
