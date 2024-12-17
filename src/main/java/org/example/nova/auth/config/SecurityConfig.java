3bf87b43fb6d6e47c4f7cdd78b047d5a7f00a3b0
김한주 <s24043@gsm.hs.kr>
2024-12-17 21:04:19 +0900

    add :: SecurityConfig.java

---
+++ src/main/java/org/example/nova/auth/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package org.example.nova.auth.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.nova.auth.entity.MemberRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/oauth/admin").hasRole(MemberRole.ADMIN.name())
+                        .requestMatchers("/oauth/info").authenticated()
+                        .anyRequest().permitAll()
+                );
+
+        http
+                .formLogin((auth) -> auth.loginPage("/oauth/login")
+                        .loginProcessingUrl("/oauth/loginProc")
+                        .usernameParameter("loginId")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/oauth")
+                        .failureUrl("/oauth")
+                        .permitAll());
+
+        http
+                .oauth2Login((auth) -> auth.loginPage("/oauth/login")
+                        .defaultSuccessUrl("/oauth")
+                        .failureUrl("/oauth/login")
+                        .permitAll());
+
+        http
+                .logout((auth) -> auth
+                        .logoutUrl("/oauth/logout"));
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+
+
+        return new BCryptPasswordEncoder();
+    }
+}
