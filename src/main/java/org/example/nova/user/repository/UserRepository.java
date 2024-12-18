b0179fb20f6b2f17a25589c94ed827261996bd02
김한주 <s24043@gsm.hs.kr>
2024-12-18 18:04:53 +0900

    add :: UserRepository.java

--- src/main/java/org/example/nova/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.nova.user.repository;
-
-import org.example.nova.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByLoginId(String loginId);
-    User findByLoginId(String loginId);
-}
