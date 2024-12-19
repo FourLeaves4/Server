93be5e95948166ad8493b2a0896828f6e962b6fc
Jeongyeon_Song <songj239587@gmail.com>
2024-12-19 20:21:57 +0900

    modify :: SecurityConfig

--- src/main/java/org/example/nova/auth/config/SecurityConfig.java
+++ src/main/java/org/example/nova/auth/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package org.example.nova.auth.config;
 
 import lombok.RequiredArgsConstructor;
-import org.example.nova.user.entity.UserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +20,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/admin").hasRole(UserRole.ADMIN.name())
-                        .requestMatchers("/auth/info").authenticated()
+                .csrf(csrf -> csrf.disable())
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
                 .formLogin(auth -> auth
@@ -36,8 +40,7 @@ public class SecurityConfig {
                         .failureUrl("/auth/login")
                         .permitAll()
                 )
-                .logout(auth -> auth.logoutUrl("/auth/logout"))
-                .csrf(auth -> auth.disable());
+                .logout(auth -> auth.logoutUrl("/auth/logout"));
 
         return http.build();
     }
@@ -45,5 +48,21 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setExposedHeaders(Arrays.asList("*"));
+        config.setMaxAge(86400L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
