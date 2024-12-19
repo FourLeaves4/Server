0c7c6acb22ccc7cd7a9860b9ab2b650c55610fad
김한주 <s24043@gsm.hs.kr>
2024-12-19 14:44:59 +0900

    modify :: SecurityConfig.java

--- src/main/java/org/example/nova/domain/auth/config/SecurityConfig.java
+++ src/main/java/org/example/nova/domain/auth/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package org.example.nova.domain.auth.config;
 
-
 import lombok.RequiredArgsConstructor;
-import org.example.nova.domain.user.entity.UserRole;
 import org.example.nova.global.verifier.CustomAuthenticationFailureHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
@@ -16,14 +12,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final ClientRegistrationRepository clientRegistrationRepository;
+    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/admin").hasRole("ADMIN")
@@ -35,7 +31,7 @@ public class SecurityConfig {
                         .loginProcessingUrl("/auth/loginProc")
                         .usernameParameter("loginId")
                         .passwordParameter("password")
-                        .defaultSuccessUrl("/auth")
+                        .defaultSuccessUrl("/auth", true)
                         .failureUrl("/auth")
                         .permitAll()
                 )
@@ -43,18 +39,17 @@ public class SecurityConfig {
                         .loginPage("/auth/login")
                         .defaultSuccessUrl("/auth", true)
                         .failureUrl("/auth/login?error=true")
-                        .failureHandler(new CustomAuthenticationFailureHandler())
+                        .failureHandler(customAuthenticationFailureHandler)
                         .authorizationEndpoint(endpoint -> endpoint
                                 .authorizationRequestRepository(authorizationRequestRepository())
-                                .authorizationRequestResolver(
-                                        new CustomAuthorizationRequestResolver(
-                                                clientRegistrationRepository,
-                                                "/oauth2/authorization"
-                                        )
-                                ))
+                                .baseUri("/oauth2/authorization")
+                        )
                         .permitAll()
                 )
-                .logout(auth -> auth.logoutUrl("/auth/logout"))
+                .logout(auth -> auth
+                        .logoutUrl("/auth/logout")
+                        .permitAll()
+                )
                 .csrf(csrf -> csrf
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 );
@@ -71,5 +66,4 @@ public class SecurityConfig {
     public HttpSessionOAuth2AuthorizationRequestRepository authorizationRequestRepository() {
         return new HttpSessionOAuth2AuthorizationRequestRepository();
     }
-
 }
