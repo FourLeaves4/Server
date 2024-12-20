b3d69254b874adfcc0a14f4291468a6d84e624ae
Jeongyeon_Song <songj239587@gmail.com>
2024-12-20 10:28:29 +0900

    add :: Post /mission

--- src/main/java/org/example/nova/home/controller/HomeController.java
+++ src/main/java/org/example/nova/home/controller/HomeController.java
@@ -1,10 +1,12 @@
 package org.example.nova.home.controller;
 
 import org.example.nova.home.dto.MissionRequestDto;
-import org.example.nova.home.dto.MissionTodayRequestDto;
 import org.example.nova.home.dto.MissionResponseDto;
+import org.example.nova.home.dto.MissionTodayRequestDto;
 import org.example.nova.home.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,13 +21,18 @@ public class HomeController {
     }
 
     @GetMapping("/mission")
-    public MissionResponseDto getMission(@PathVariable("user_id") Long userId, @RequestBody MissionRequestDto missionRequestDto) {
-        return homeService.getMissionByUserId(userId, missionRequestDto);
+    public MissionResponseDto getMission(@PathVariable("user_id") Long userId) {
+        return homeService.getMissionByUserId(userId);
     }
 
     @PostMapping("/mission")
-    public String updateMission(@PathVariable("user_id") Long userId, @RequestBody MissionTodayRequestDto missionTodayRequestDto) {
+    public MissionResponseDto updateMission(@PathVariable("user_id") Long userId, @RequestBody MissionRequestDto missionRequestDto) {
+        return homeService.getMissionByUserId(userId, missionRequestDto);
+    }
+
+    @PostMapping("/mission/value")
+    public ResponseEntity<Void> updateMissionValue(@PathVariable("user_id") Long userId, @RequestBody MissionTodayRequestDto missionTodayRequestDto) {
         homeService.updateMission(userId, missionTodayRequestDto);
-        return "Mission updated successfully for user_id: " + userId;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
