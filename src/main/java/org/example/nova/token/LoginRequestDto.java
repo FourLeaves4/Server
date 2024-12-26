9db35193b25bf30b450e6f0034061957d0f77c04
김한주 <s24043@gsm.hs.kr>
2024-12-27 08:54:45 +0900

    add :: LoginRequestDto.java

---
+++ src/main/java/org/example/nova/token/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package org.example.nova.token;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
