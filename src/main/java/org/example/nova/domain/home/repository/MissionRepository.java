4452917a32d11076bd6fcd3c4d940be37bd7b5e5
김한주 <s24043@gsm.hs.kr>
2024-12-28 00:07:22 +0900

    modify :: MissionRepository.java

--- src/main/java/org/example/nova/domain/home/repository/MissionRepository.java
+++ src/main/java/org/example/nova/domain/home/repository/MissionRepository.java
@@ -1,6 +1,6 @@
-package org.example.nova.home.repository;
+package org.example.nova.domain.home.repository;
 
-import org.example.nova.home.entity.Mission;
+import org.example.nova.domain.home.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
