d435176d89cd42b93dc7401a976897daeba152d6
김한주 <s24043@gsm.hs.kr>
2024-12-27 23:24:18 +0900

    add :: ErrorResponse.java

---
+++ src/main/java/org/example/nova/global/dto/response/ErrorResponse.java
@@ -0,0 +1,18 @@
+package org.example.nova.global.dto.response;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    @JsonProperty("http_code")
+    private int httpCode;
+    private String message;
+
+}
