5f3c5e34adf64b33b6027785a036df1abd412a72
김한주 <s24043@gsm.hs.kr>
2024-12-27 23:56:42 +0900

    add :: MissionService.java

---
+++ src/main/java/org/example/nova/domain/auth/service/MissionService.java
@@ -0,0 +1,10 @@
+package org.example.nova.domain.auth.service;
+
+import org.example.nova.domain.auth.entity.User;
+
+public interface MissionService {
+
+    void initializeMissionAndProfile(User user);
+
+}
+
