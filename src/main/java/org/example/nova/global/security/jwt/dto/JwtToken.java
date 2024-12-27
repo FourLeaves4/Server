81139d7f3844a30d40261743edbf7fdb323a921b
김한주 <s24043@gsm.hs.kr>
2024-12-27 23:16:39 +0900

    add :: JwtToken.java

---
+++ src/main/java/org/example/nova/global/security/jwt/dto/JwtToken.java
@@ -0,0 +1,15 @@
+package org.example.nova.global.security.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtToken {
+
+    private String accessToken;
+    private String refreshToken;
+    private String accessTokenExpiresIn;
+    private String refreshTokenExpiresIn;
+
+}
