85e709b18008f0714e318058fd6e2aabde7efe0a
김한주 <s24043@gsm.hs.kr>
2024-12-27 23:24:51 +0900

    add :: TokenResponse.java

---
+++ src/main/java/org/example/nova/global/dto/response/TokenResponse.java
@@ -0,0 +1,20 @@
+package org.example.nova.global.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private LocalDateTime accessTokenExpiresIn;
+    private LocalDateTime refreshTokenExpiresIn;
+
+}
