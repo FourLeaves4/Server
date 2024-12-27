ece3e806044e5131cceea35d91a3d8d9cdfd448a
김한주 <s24043@gsm.hs.kr>
2024-12-27 23:48:39 +0900

    modify :: UserRepository.java

--- src/main/java/org/example/nova/domain/auth/repository/UserRepository.java
+++ src/main/java/org/example/nova/domain/auth/repository/UserRepository.java
@@ -1,13 +1,15 @@
-package org.example.nova.auth.repository;
+package org.example.nova.domain.auth.repository;
 
-import org.example.nova.auth.entity.User;
+import org.example.nova.domain.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByLoginId(String loginId);
     Optional<User> findByUserId(Long userId);
     Optional<User> findByEmail(String email);
     Optional<User> findByName(String name);
+
 }
