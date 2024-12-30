a2d1b38fdb996734ee3a876f99e0c7aa4c603406
김한주 <s24043@gsm.hs.kr>
2024-12-30 23:02:49 +0900

    modify :: SecurityConfig.java

--- src/main/java/org/example/nova/global/security/config/SecurityConfig.java
+++ src/main/java/org/example/nova/global/security/config/SecurityConfig.java
@@ -1,17 +1,6 @@
 package org.example.nova.global.security.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+/*
 
 @Configuration
 @EnableWebSecurity
@@ -65,9 +54,24 @@ public class SecurityConfig {
     }
 }
 
-/*
+ */
 
-// permitAll() <-- DELETE
+
+import lombok.RequiredArgsConstructor;
+import org.example.nova.global.security.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
@@ -123,4 +127,3 @@ public class SecurityConfig {
     }
 }
 
- */
