403f377bd3612fbfd8f68eeaaf423d2bc2394724
김한주 <s24043@gsm.hs.kr>
2024-12-30 15:21:02 +0900

    modify :: CustomException.java

--- src/main/java/org/example/nova/global/security/jwt/exception/CustomException.java
+++ src/main/java/org/example/nova/global/security/jwt/exception/CustomException.java
@@ -1,13 +1,27 @@
 package org.example.nova.global.security.jwt.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException {
-    private final ErrorCode errorCode;
 
-    public CustomException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
+    private final ErrorCode errorCode;
+    private final String message;
+
+    public CustomException(ErrorCode errorCode, String message) {
+        super(message);
         this.errorCode = errorCode;
+        this.message = message;
     }
+
+    public HttpStatus getStatus() {
+        return errorCode.getStatus();
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
 }
