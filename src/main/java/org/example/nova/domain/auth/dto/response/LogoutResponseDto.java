1e5c9c7c8b56e11934f8e2d5f49b75644f6ba910
김한주 <s24043@gsm.hs.kr>
2024-12-30 15:16:44 +0900

    modify :: LogoutResponseDto.java

--- src/main/java/org/example/nova/domain/auth/dto/response/LogoutResponseDto.java
+++ src/main/java/org/example/nova/domain/auth/dto/response/LogoutResponseDto.java
@@ -1,5 +1,13 @@
 package org.example.nova.domain.auth.dto.response;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LogoutResponseDto {
 
   private String message;
