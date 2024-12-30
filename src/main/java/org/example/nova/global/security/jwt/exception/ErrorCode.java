3fa662622d574fe49b38a73445b43cfcfb2ff7ac
김한주 <s24043@gsm.hs.kr>
2024-12-30 15:21:40 +0900

    modify :: ErrorCode.java

--- src/main/java/org/example/nova/global/security/jwt/exception/ErrorCode.java
+++ src/main/java/org/example/nova/global/security/jwt/exception/ErrorCode.java
@@ -10,11 +10,8 @@ public enum ErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다."),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
-    INVALID_EMAIL(HttpStatus.BAD_REQUEST, "유효하지 않은 이메일 형식입니다.");
-
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "사용자 저장 중 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String message;
